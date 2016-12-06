bc423954ce719039443abe117bb88418ec56d467
fukuta0614 <fukuta6140@gmail.com>
2016-12-06 13:27:14 +0900

    とりあえずSL回せるように

--- models/rl_policy.py
+++ models/rl_policy.py
@@ -15,7 +15,7 @@ def softmax(x, T):
 class RLPolicy(chainer.Chain):
     """Supervised learning policy network"""
 
-    def __init__(self,density=1, size=8, channel=6):
+    def __init__(self, density=1, size=8, channel=6):
         """
         黒[0,1] | 白[0,1] | empty[0, 1] | 置いた場合にひっくり返る個数 | 手番が黒か否か | ターン数 |
 
@@ -31,29 +31,30 @@ class RLPolicy(chainer.Chain):
             norm2=L.BatchNormalization(16 * density),
             conv3=L.Convolution2D(16 * density, 16 * density, 4, stride=1, pad=1),
             norm3=L.BatchNormalization(16 * density),
-            linear1=L.Linear(400*density, 400*density),
-            linear2=L.Linear(300*density, 64)
+            linear1=L.Linear(400 * density, 400 * density),
+            linear2=L.Linear(400 * density, 64)
         )
 
+    # noinspection PyUnresolvedReferences,PyCallingNonCallable
     def predict(self, x, train=True):
-
         h1 = F.relu(self.norm1(self.conv1(x), test=not train))
         h2 = F.relu(self.norm2(self.conv2(h1), test=not train))
         h3 = F.relu(self.norm3(self.conv3(h2), test=not train))
         h4 = F.relu(self.linear1(h3))
-        scores = self.linear(h4)
+        scores = self.linear2(h4)
         return scores
 
+    # noinspection PyCallingNonCallable
     def __call__(self, x, ply):
         scores = self.predict(x)
         self.loss = F.softmax_cross_entropy(scores, ply)
 
         return self.loss
 
+    # noinspection PyCallingNonCallable
     def act(self, x, temperature=1):
         scores = self.predict(x)
         pred = softmax(cuda.to_cpu(scores.data), T=temperature)
         action = [np.random.choice(64, p=p) for p in pred]
 
         return action
-
