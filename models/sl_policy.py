33bcf061347eddf622338def161703c704536947
fukuta0614 <fukuta6140@gmail.com>
2016-12-20 18:39:36 +0900

    sl系全部直しました

--- models/sl_policy.py
+++ models/sl_policy.py
@@ -5,6 +5,10 @@ from chainer import cuda
 from chainer import reporter
 import numpy as np
 
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from reversi import board
+
 
 def softmax(x, T):
     y = x - x.max(axis=1, keepdims=True)
@@ -16,7 +20,7 @@ def softmax(x, T):
 class SLPolicy(chainer.Chain):
     """Supervised learning policy network"""
 
-    def __init__(self, density=1, channel=6, use_bn=False):
+    def __init__(self, density=1, channel=6, use_bn=True):
         """
         黒[0,1] | 白[0,1] | valid[0, 1] | 置いた場合にひっくり返る個数 | ターン数 | 手番
 
@@ -25,6 +29,7 @@ class SLPolicy(chainer.Chain):
         """
 
         self.use_bn = use_bn
+        self.train = True
         layers = dict()
 
         layers['conv1'] = L.Convolution2D(channel, 8 * density, 1, stride=1, pad=0)
@@ -33,10 +38,10 @@ class SLPolicy(chainer.Chain):
         layers['conv4'] = L.Convolution2D(32 * density, 32 * density, 3, stride=1, pad=1)
 
         if self.use_bn:
-            layers['norm1'] = L.BatchNormalization(8 * density)
             layers['norm2'] = L.BatchNormalization(16 * density)
             layers['norm3'] = L.BatchNormalization(32 * density)
             layers['norm4'] = L.BatchNormalization(32 * density)
+            layers['norm5'] = L.BatchNormalization(1024 * density)
 
         layers['linear1'] = L.Linear(2048 * density, 1024 * density)
         layers['linear2'] = L.Linear(1024 * density, 1024 * density)
@@ -47,11 +52,11 @@ class SLPolicy(chainer.Chain):
     # noinspection PyUnresolvedReferences,PyCallingNonCallable
     def predict(self, x, train=True):
         if self.use_bn:
-            h1 = F.relu(self.norm1(self.conv1(x), test=not train))
+            h1 = F.relu(self.conv1(x))
             h2 = F.relu(self.norm2(self.conv2(h1), test=not train))
             h3 = F.relu(self.norm3(self.conv3(h2), test=not train))
             h4 = F.relu(self.norm4(self.conv4(h3), test=not train))
-            h5 = F.relu(self.linear1(h4))
+            h5 = F.relu(self.norm5(self.linear1(h4), test=not train))
             h6 = F.relu(self.linear2(h5))
             scores = self.linear3(h6)
 
@@ -68,20 +73,23 @@ class SLPolicy(chainer.Chain):
             return scores
 
     # noinspection PyCallingNonCallable
-    def __call__(self, x, ply):
-        scores = self.predict(x)
+    def __call__(self, x, ply, train=True):
+        scores = self.predict(x, train)
         self.loss = F.softmax_cross_entropy(scores, ply)
-        reporter.report({'loss': self.loss}, self)
+        # reporter.report({'loss': self.loss}, self)
 
         self.accuracy = F.accuracy(scores, ply)
-        reporter.report({'accuracy': self.accuracy}, self)
-
-        return self.loss
+        # reporter.report({'accuracy': self.accuracy}, self)
+        if train:
+            return self.loss
+        else:
+            return np.argmax(cuda.to_cpu(scores.data), axis=1)
 
     # noinspection PyCallingNonCallable
-    def act(self, x, temperature=1):
+    def act(self, b, color, turn, temperature=1):
+        x = board.to_state(b, color, turn)
+        x = chainer.Variable(self.xp.array([x], 'float32'), volatile=True)
         scores = self.predict(x)
         pred = softmax(cuda.to_cpu(scores.data), T=temperature)
-        action = [np.random.choice(64, p=p) for p in pred]
-
+        action = np.random.choice(64, p=pred[0])
         return action
