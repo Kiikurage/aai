2c2f3acd7054d412332db5621e3f77c7c08e4207
飯塚達哉 <iizuka.tatsu@gmail.com>
2016-12-27 19:06:34 +0900

    GUI用

---
+++ othello-js-master/server.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, json
+import selector
+
+app = Flask(__name__,static_folder='.',static_url_path='')
+
+@app.route('/')
+def home():
+    print("return index.html")
+    return app.send_static_file('index.html')
+
+@app.route('/getMove', methods=['POST'])
+def face_info():
+    if request.headers['Content-Type'] != 'application/json':
+        print("json contents")
+        print(request.headers['Content-Type'])
+        return json.jsonify(res='error'), 400
+
+    #print request.jso
+    gameTree = request.get_json()
+    #print(posted_data["test_value"])
+    bestMove = selector.selectMove(gameTree["board"],gameTree["moves"],gameTree["player"])
+    return json.jsonify(index=bestMove)
+
+'''
+@app.route('/echo/<thing>')
+def echo(thing):
+    return thing
+'''
+app.run(port=9999,debug=True)
